bfad1967df185be5a76f04304fbe928667535e97
doa-b <doa.bonifacio@gmail.com>
2019-06-24 16:49:43 +0200

    Added necessary Repositories

---
+++ src/main/java/guru/springframework/Repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.Repositories;
+
+import guru.springframework.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
